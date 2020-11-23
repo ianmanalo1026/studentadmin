eef505b7f23eea6c9557facbd457373152938d79
Ian <ian.manalo1026@gmail.com>
2020-11-23 23:06:55 +0800

    new project

---
+++ student/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from student.models import Profile, Course, Subject
+
+
+admin.site.register(Profile)
+admin.site.register(Course)
+admin.site.register(Subject)
---
+++ student/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=20)
+    description = models.TextField(blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Subject(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    school_year = models.SmallIntegerField()
+    start_time = models.TimeField()
+    room = models.CharField(max_length=50)
+    unit = models.SmallIntegerField()
+    
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    student = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    course = models.OneToOneField('Course', on_delete=models.CASCADE, null=True)
+    subject = models.ManyToManyField('Subject')
+    
+    def __str__(self):
+        return str(self.student)
